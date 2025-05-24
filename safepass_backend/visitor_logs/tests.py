c3e2f74cc5793b88e56cb76efd327342d1cbe963
Ryfiel <ryfielvgonzales@gmail.com>
2025-05-24 12:00:29 +0800

    finished visitor_logs views testing

--- safepass_backend/visitor_logs/tests.py
+++ safepass_backend/visitor_logs/tests.py
@@ -7,31 +7,31 @@ from datetime import time, date
 class VisitorLogsTest(APITestCase):
     def setUp(self):
         purpose = models.VisitPurposes.objects.create(
-        purpose ="Visit"
+            purpose ="Visit"
         )
         status = models.visitor_status.objects.create(
-        status ="Denied"
+            status ="Denied"
         )
         id_type = visitor_details_models.IdTypes.objects.create(
-        type = "Passport" 
+            type = "Passport" 
         )
         visitor_details = visitor_details_models.VisitorDetails.objects.create(
-        first_name = "Ryfiel Sean",
-        middle_name = "Viray",
-        last_name = "Gonzales",
-        contact_number = "09777237273",
-        id_type = id_type,
-        id_number = "23671986387216",
-        status = "Approved",
+            first_name = "Ryfiel Sean",
+            middle_name = "Viray",
+            last_name = "Gonzales",
+            contact_number = "09777237273",
+            id_type = id_type,
+            id_number = "23671986387216",
+            status = "Approved"
         )
 
         models.visitor_logs.objects.create(
-        visitor_details = visitor_details,
-        check_in=time(9, 0),     
-        check_out=time(11, 30),  
-        visit_date=date(2025, 5, 21),
-        purpose = purpose,
-        status = status,
+            visitor_details = visitor_details,
+            check_in=time(9, 0),     
+            check_out=time(11, 30),  
+            visit_date=date(2025, 5, 21),
+            purpose = purpose,
+            status = status,
         )
         self.client = APIClient()
     def test_logs(self):
